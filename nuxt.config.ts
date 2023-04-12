1c7ebb65d29062ba539bf5e7ad227d6873292bbc
Péah <padecallonne@gmail.com>
2023-04-12 15:23:23 +0200

    modification du deploy yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL : 'peahrar.github.io',
+        baseURL : 'peahrar.github.io/discoverNuxt/',
         head: {
             script: [{
                 src: "https://www.youtube.com/iframe_api",
